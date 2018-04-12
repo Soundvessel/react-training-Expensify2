e1be334247a876366c04f37dcf6f2c7206a4fd37
Matthew Giacomazzo <g@coplex.com>
2018-04-12 16:03:08 -0700

    Bugfix of FOUC issue with _document.js https://github.com/zeit/next.js/issues/1236

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,8 @@ import flush from 'styled-jsx/server'
 export default class CustomDocument extends Document {
   static getInitialProps({ renderPage }) {
     const { html, head, errorHtml, chunks } = renderPage()
-    return { html, head, errorHtml, chunks }
+    const styles = flush()
+    return { html, head, errorHtml, chunks, styles }
   }
 
   render() {
@@ -15,13 +16,6 @@ export default class CustomDocument extends Document {
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <link rel="stylesheet" href="/_next/static/style.css" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png"/>
-          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png"/>
-          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png"/>
-          <link rel="manifest" href="/static/favicons/manifest.json"/>
-          <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#24afdf"/>
-          <link rel="shortcut icon" href="/static/favicons/favicon.ico"/>
-          <meta name="msapplication-config" content="/static/favicons/browserconfig.xml"/>
         </Head>
         <body>
           <Main/>
